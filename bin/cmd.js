364cc9876c3b99884299b58b29547379c27109ec
Ezo Saleh <ezo@the-experimenters.com>
2016-05-12 21:42:58 +0100

    Created tt-translate skeleton.

--- bin/cmd.js
+++ bin/cmd.js
@@ -2,15 +2,36 @@
 
 'use strict';
 
-const program       = require('commander')
-    , chalk         = require('chalk');
+const program   = require('commander')
+    , chalk     = require('chalk')
+    , fs        = require('fs')
+    , path      = require('path')
+    , h         = require('highland')
+    , mkdirp    = require('mkdirp')
+    , translate = require('../');
 
 program
 .usage('[options] <statement.(ofx|pdf|csv)>')
 .description('Part of the Tell Tally suite. Translates UK bank statements into a legible JSON format.')
-.arguments('<file>')
-.action((file) => {
-  console.log('Hello World!');
+.arguments('<statement>')
+.option('-o, --out <directory-path>', 'Optional output directory (defaults to \'translations\').')
+.action( statement => {
+  const outputDirectory = program.out || 'translations';
+  mkdirp.sync(outputDirectory);
+
+  const writeStream = h(fs.createWriteStream(path.join(outputDirectory, 'charges.json')));
+  const source = translate(statement);
+  source
+  .streamJson()
+  .doto(console.log)
+  .pipe(fs.createWriteStream(path.join(outputDirectory, 'charges.json')))
+  .on('error', (err) => {
+    console.error('err', err);
+  })
+  .on('finish', () => {
+    console.log('Charges ready!');
+  });
+
 })
 .parse(process.argv);
 
--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 'use strict';
 
-module.exports = {};
+const h         = require('highland')
+    , stringify = require('highland-json').stringify;
+
+module.exports = Translator;
+
+function Translator(statement) {
+  if (!(this instanceof Translator)) return new Translator(statement);
+  this.parse(statement);
+}
+
+Translator.prototype.parse = function (statement) {
+  return this;
+};
+
+Translator.prototype.streamJson = function () {
+  const input = [
+    { a : "b", b : "c" },
+    { a : [1, 2, 3] },
+    [ 1, 2, 3 ],
+    [ { a : "b", b : "c" } ]
+  ];
+
+  return h(input).through(stringify);
+};
